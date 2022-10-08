84a08b84af6d9240ba7f3d3f4adfbe1622521c4c
sihagwak <sasha.gwak@gmail.com>
2022-10-08 22:59:00 +0900

    feat: signup mongodb 넣기 구현

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const app = express();
 /* bodyParser */
 const bodyParser = require('body-parser'); 
 app.use(bodyParser.urlencoded({ extended: false })); 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 /* cors */
 var cors = require('cors');
@@ -17,30 +17,37 @@ const data = {
 
 /* DB */ 
 const mongoose = require('mongoose');
-const MONGODB_URI = 'mongodb+srv://poemha:<password>!@clustertest.bwpwhd8.mongodb.net/Cat';
+const MONGODB_URI = 'mongodb+srv://poemha:Mini1028!@clustertest.bwpwhd8.mongodb.net/Cat';
+const User = require('./models/user');
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
 }); 
 
-app.get('/api/user/login', (req, res) => {
+app.get('/api/user/signup', (req, res) => {
   res.json(data);
 }); 
 
-app.post('/api/user/login', (req, res) => {
+app.post('/api/user/signup', async (req, res) => {
   const { id , password, email} = req.body;
+  const user = new User({
+    id: id, 
+    email: email, 
+    password: password
+  }); 
+  await user.save(err => {console.log(err)});
   console.log(id, email, password);
-  res.send('Success');
+  return res.send('Success');
 }); 
 
-// mongoose.connect(MONGODB_URI)
-//   .then(result => {
-//     app.listen(8000, () => {
-//       console.log('Server start!');
-//     });
-//   })
-//   .catch(err => console.log(err)); 
+mongoose.connect(MONGODB_URI)
+  .then(result => {
+    app.listen(8000, () => {
+      console.log('Server start!');
+    });
+  })
+  .catch(err => console.log(err)); 
 
-app.listen(8000, () => {
-  console.log('Server start!');
-});
+// app.listen(8000, () => {
+//   console.log('Server start!');
+// });
