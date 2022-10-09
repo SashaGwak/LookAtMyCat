c91bdee07a3bce799d5ac6aa3ddd99bfeceee614
sihagwak <sasha.gwak@gmail.com>
2022-10-10 00:50:53 +0900

    feat: 로그인 기능 추가

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -10,10 +10,17 @@ router.get('/api/user/signup', (req, res) => {
   }); 
 
 /* 비밀번호 암호화 함수 */
+const salt = crypto.randomBytes(64).toString('base64'); 
 const createHashedPassword = (password) => {
-    const salt = crypto.randomBytes(64).toString('base64'); 
     return crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('base64');
 };
+/* 비밀번호 확인 함수 */
+const verifyPassword = (password, salt, userPassword) => {
+  const hashed = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('base64');
+
+  if (hashed == userPassword) return true; 
+  return false; 
+}
 
 /* 회원가입 기능 */
 router.post('/signup', async (req, res) => {
@@ -28,5 +35,18 @@ router.post('/signup', async (req, res) => {
     await user.save(err => {console.log(err)});
     return res.send({isCreate: true});
 }); 
+
+/* 로그인 기능 */
+router.post('/login', async(req, res) => {
+  const { id, password } = req.body; 
+  User.findOne({id: id})
+  .then(data => {
+    if (verifyPassword( password, salt, data.password)) {
+      return res.send({isLogin: true});
+    } else {
+      return res.send({isLogin: false});
+    }
+  })
+});
 
 module.exports = router;
