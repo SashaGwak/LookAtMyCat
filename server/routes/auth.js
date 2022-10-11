a00cb79133ac998f6a51ed1ae727afb6b45f9e4c
sihagwak <sasha.gwak@gmail.com>
2022-10-11 13:59:33 +0900

    feat: passport 기능 추가

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,6 +4,7 @@ const crypto = require('crypto');
 
 const User = require('../models/user'); 
 
+/* 로그인 확인 미들웨어 */
 function isLoggedIn(req, res, next) {
   req.user ? next() : res.sendStatus(401);
 }
@@ -19,23 +20,17 @@ router.get('/google', (req, res) => {
 
 /* 패스포트 로그인 기능 */
 router.get('/google/login', 
-  passport.authenticate('google', { scope: ['email', 'profile']})
+  // 프로파일과 이메일 정보를 받는다.
+  passport.authenticate('google', { scope: ['email', 'profile']}) 
 )
 
+/* 패스포트 로그인 후 라우터 */
 router.get('/google/callback', 
   passport.authenticate('google', {
-    successRedirect: 'protected', 
-    failureRedirect: 'google/failure'
+    successRedirect: 'http://localhost:3000/', 
+    failureRedirect: 'http://localhost:3000/api/user/login', // 로그인 실패시
   })
 )
-
-router.get('/google/protected', isLoggedIn, (req, res) => {
-  res.send('Hello!');
-});
-
-router.get('google/failure', isLoggedIn, (req, res) => {
-  res.send('something worng...!')
-});
 
 /* 회원가입 페이지 */
 router.get('/signup', (req, res) => {
@@ -83,75 +78,11 @@ router.post('/login', async(req, res) => {
   })
 });
 
-
-// /* google passport */
-// const passport = require('passport');
-// const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
-// module.exports = () => {
-//   passport.use(
-//     new GoogleStrategy(
-//       {
-//         id : process.env.GOOGLE_ID, // 구글 로그인 REST API 키 
-//         clientSecret: process.env.GoogleStrategy, 
-//         callback: 'api/user/google', // 구글 로그인 Redirect URI 경로
-//       }, 
-//       async (accessTocke, refreshToken, profile, done) => {
-//         console.log('google profie: ', profile);
-//         try {
-//           const exUser = await User.findOne({
-//             // 구글 플랫폼에서 로그인 했고 & snsId필드에 구글 아이디가 일치할경우
-//             where: {snsId: profile.id, provider: 'google'}, 
-//           }); 
-//           // 이미 가입된 구글 프로필이면 성공
-//           if (exUser){
-//             done(null, exUser); // 로그인 인증 완료
-//           } else {
-//             // 가입되지 않는 유저면 회원가입 시키고 로그인을 시킨다
-//             const newUser = await User.create({
-//               email: profile?.email[0].value,
-//               nick: profile.displayName,
-//               snsId: profile.id,
-//               provider: 'google',
-//             });
-//             done(null, newUser); // 회원가입하고 로그인 인증 완료
-//           }
-//         } catch (error) {
-//           console.error(error);
-//           done(error)
-//         }
-//       }, 
-//     ), 
-//   );
-// };
-
-// /* 구글 로그인 */
-// router.get('user/google', passport.authenticate('google', { scope: ['profile', 'email']})); 
-
-// router.get(
-//   'user/google/callback', 
-//   passport.authenticate('google', {failureRedirect: '/'}), // passport 로그인 전략에 의해 strategy로 가서 구글 계정 정보와 DB를 비교해서 회원가입 시키거나 로그인 하도록 함 
-//   (req, res) => {
-//     res.redirect('/');
-//   }
-// )
-
-// module.exports = () => { 
-//    passport.serializeUser((user, done) => {
-//       done(null, user.id);
-//    });
-//    passport.deserializeUser((id, done) => {
-//       User.findOne({ where: { id } })
-//         .then(user => {
-//           console.log(user);
-//           done(null, user)
-
-//         })
-//          .catch(err => done(err));
-//    }); 
-//    local();
-//    google(); // 구글 전략 등록
-// };
-
+/* 로그아웃 기능 */
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.send('Goodbye!');
+});
 
 module.exports = router;
